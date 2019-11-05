7fdf99ea326d42350f0e5990525207628aad52e9
Tan Jiann Meng <jiannmeng@fi.life>
2019-11-05 16:37:25 +0800

    Add tests for BaseRate class.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,11 @@
 import responses
 import pytest
 
-from main import BnmpyItem, BASE_URL
+from main import BnmpyItem, BaseRate, BASE_URL
 
 
 @pytest.fixture
-def mock_responses():
+def mock_basic():
     with responses.RequestsMock(assert_all_requests_are_fired=False) as r:
         r.add(
             responses.GET, BASE_URL + "none", json={"data": None}, status=200,
@@ -37,31 +37,349 @@ def mock_responses():
         yield r
 
 
-def test_single(mock_responses):
-    b = BnmpyItem("single")
-    assert b.data == [{"hello": "world"}]
+@pytest.fixture
+def mock_base_rate():
+    with responses.RequestsMock(assert_all_requests_are_fired=False) as r:
+        r.add(
+            responses.GET,
+            BASE_URL + "base-rate",
+            json={
+                "data": [
+                    {
+                        "bank_code": "BKKBMYKL",
+                        "bank_name": "Bangkok Bank Berhad",
+                        "base_rate": 4.47,
+                        "base_lending_rate": 7.14,
+                        "indicative_eff_lending_rate": 5.67,
+                    },
+                    {
+                        "bank_code": "CIBBMYKL",
+                        "bank_name": "CIMB Bank Berhad",
+                        "base_rate": 4,
+                        "base_lending_rate": 6.85,
+                        "indicative_eff_lending_rate": 4.75,
+                    },
+                    {
+                        "bank_code": "CITIMYKL",
+                        "bank_name": "Citibank Berhad",
+                        "base_rate": 3.65,
+                        "base_lending_rate": 6.8,
+                        "indicative_eff_lending_rate": 4.45,
+                    },
+                    {
+                        "bank_code": "HLBBMYKL",
+                        "bank_name": "Hong Leong Bank Malaysia Berhad",
+                        "base_rate": 3.88,
+                        "base_lending_rate": 6.89,
+                        "indicative_eff_lending_rate": 4.75,
+                    },
+                    {
+                        "bank_code": "HBMBMYKL",
+                        "bank_name": "HSBC Bank Malaysia Berhad",
+                        "base_rate": 3.64,
+                        "base_lending_rate": 6.74,
+                        "indicative_eff_lending_rate": 4.75,
+                    },
+                    {
+                        "bank_code": "ICBKMYKL",
+                        "bank_name": "Industrial and Commercial Bank of China (Malaysia) Berhad",
+                        "base_rate": 3.77,
+                        "base_lending_rate": 6.7,
+                        "indicative_eff_lending_rate": 4.6,
+                    },
+                    {
+                        "bank_code": "MBBEMYKL",
+                        "bank_name": "Malayan Banking Berhad",
+                        "base_rate": 3,
+                        "base_lending_rate": 6.65,
+                        "indicative_eff_lending_rate": 4.35,
+                    },
+                    {
+                        "bank_code": "OCBCMYKL",
+                        "bank_name": "OCBC Bank (Malaysia) Berhad",
+                        "base_rate": 3.83,
+                        "base_lending_rate": 6.76,
+                        "indicative_eff_lending_rate": 4.7,
+                    },
+                    {
+                        "bank_code": "PBBEMYKL",
+                        "bank_name": "Public Bank Berhad",
+                        "base_rate": 3.52,
+                        "base_lending_rate": 6.72,
+                        "indicative_eff_lending_rate": 4.35,
+                    },
+                    {
+                        "bank_code": "RHBBMYKL",
+                        "bank_name": "RHB Bank Berhad",
+                        "base_rate": 3.75,
+                        "base_lending_rate": 6.7,
+                        "indicative_eff_lending_rate": 4.75,
+                    },
+                    {
+                        "bank_code": "SCBLMYKX",
+                        "bank_name": "Standard Chartered Bank Malaysia Berhad",
+                        "base_rate": 3.52,
+                        "base_lending_rate": 6.7,
+                        "indicative_eff_lending_rate": 4.52,
+                    },
+                    {
+                        "bank_code": "UOVBMYKL",
+                        "bank_name": "United Overseas Bank (Malaysia) Bhd.",
+                        "base_rate": 3.86,
+                        "base_lending_rate": 6.82,
+                        "indicative_eff_lending_rate": 4.61,
+                    },
+                    {
+                        "bank_code": "AIBBMYKL",
+                        "bank_name": "Affin Islamic Bank Berhad",
+                        "base_rate": 3.95,
+                        "base_financing_rate": 6.81,
+                        "indicative_eff_lending_rate": 4.76,
+                    },
+                    {
+                        "bank_code": "ALSRMYKL",
+                        "bank_name": "Alliance Islamic Bank Berhad",
+                        "base_rate": 3.82,
+                        "base_financing_rate": 6.67,
+                        "indicative_eff_lending_rate": 4.36,
+                    },
+                    {
+                        "bank_code": "AISLMYKL",
+                        "bank_name": "AmBank Islamic Berhad",
+                        "base_rate": 3.85,
+                        "base_financing_rate": 6.7,
+                        "indicative_eff_lending_rate": 4.5,
+                    },
+                    {
+                        "bank_code": "BIMBMYKL",
+                        "bank_name": "Bank Islam Malaysia Berhad",
+                        "base_rate": 3.77,
+                        "base_financing_rate": 6.72,
+                        "indicative_eff_lending_rate": 4.57,
+                    },
+                    {
+                        "bank_code": "BMMBMYKL",
+                        "bank_name": "Bank Muamalat Malaysia Berhad",
+                        "base_rate": 3.81,
+                        "base_financing_rate": 6.81,
+                        "indicative_eff_lending_rate": 4.81,
+                    },
+                    {
+                        "bank_code": "CTBBMYKL",
+                        "bank_name": "CIMB Islamic Bank Berhad",
+                        "base_rate": 4,
+                        "base_financing_rate": 6.85,
+                        "indicative_eff_lending_rate": 4.75,
+                    },
+                    {
+                        "bank_code": "HLIBMYKL",
+                        "bank_name": "Hong Leong Islamic Bank Berhad",
+                        "base_rate": 3.88,
+                        "base_financing_rate": 6.89,
+                        "indicative_eff_lending_rate": 4.6,
+                    },
+                    {
+                        "bank_code": "HMABMYKL",
+                        "bank_name": "HSBC Amanah Malaysia Berhad",
+                        "base_rate": 3.64,
+                        "base_financing_rate": 6.74,
+                        "indicative_eff_lending_rate": 4.75,
+                    },
+                    {
+                        "bank_code": "KFHOMYKL",
+                        "bank_name": "Kuwait Finance House (Malaysia) Berhad",
+                        "base_rate": 3.5,
+                        "base_financing_rate": 7.39,
+                        "indicative_eff_lending_rate": 4.45,
+                    },
+                    {
+                        "bank_code": "MBISMYKL",
+                        "bank_name": "Maybank Islamic Berhad",
+                        "base_rate": 3,
+                        "base_financing_rate": 6.65,
+                        "indicative_eff_lending_rate": 4.35,
+                    },
+                    {
+                        "bank_code": "AFBQMYKL",
+                        "bank_name": "MBSB Bank Berhad",
+                        "base_rate": 3.9,
+                        "base_financing_rate": 6.75,
+                        "indicative_eff_lending_rate": 4.45,
+                    },
+                    {
+                        "bank_code": "OABBMYKL",
+                        "bank_name": "OCBC Al-Amin Bank Berhad",
+                        "base_rate": 3.83,
+                        "base_financing_rate": 6.76,
+                        "indicative_eff_lending_rate": 4.7,
+                    },
+                    {
+                        "bank_code": "PUIBMYKL",
+                        "bank_name": "Public Islamic Bank Berhad",
+                        "base_rate": 3.52,
+                        "base_financing_rate": 6.72,
+                        "indicative_eff_lending_rate": 4.35,
+                    },
+                    {
+                        "bank_code": "RHBAMYKL",
+                        "bank_name": "RHB Islamic Bank Berhad",
+                        "base_rate": 3.75,
+                        "base_financing_rate": 6.7,
+                        "indicative_eff_lending_rate": 4.75,
+                    },
+                    {
+                        "bank_code": "SCSRMYKK",
+                        "bank_name": "Standard Chartered Saadiq Berhad",
+                        "base_rate": 3.52,
+                        "base_financing_rate": 6.7,
+                        "indicative_eff_lending_rate": 4.52,
+                    },
+                    {
+                        "bank_code": "AGOBMYKL",
+                        "bank_name": "Agrobank",
+                        "base_rate": 3.6,
+                        "base_financing_rate": 6.75,
+                        "indicative_eff_lending_rate": None,
+                    },
+                    {
+                        "bank_code": "BSNAMYK1",
+                        "bank_name": "Bank Simpanan Nasional",
+                        "base_rate": 3.85,
+                        "base_financing_rate": 6.6,
+                        "indicative_eff_lending_rate": 4.45,
+                    },
+                    {
+                        "bank_code": "PHBMMYKL",
+                        "bank_name": "Affin Bank Berhad",
+                        "base_rate": 3.95,
+                        "base_lending_rate": 6.81,
+                        "indicative_eff_lending_rate": 4.76,
+                    },
+                    {
+                        "bank_code": "MFBBMYKL",
+                        "bank_name": "Alliance Bank Malaysia Berhad",
+                        "base_rate": 3.82,
+                        "base_lending_rate": 6.67,
+                        "indicative_eff_lending_rate": 4.36,
+                    },
+                    {
+                        "bank_code": "ARBKMYKL",
+                        "bank_name": "AmBank (M) Berhad",
+                        "base_rate": 3.85,
+                        "base_lending_rate": 6.7,
+                        "indicative_eff_lending_rate": 4.5,
+                    },
+                    {
+                        "bank_code": "BKCHMYKL",
+                        "bank_name": "Bank of China (M) Berhad",
+                        "base_rate": 3.8,
+                        "base_lending_rate": 6.6,
+                        "indicative_eff_lending_rate": 4.8,
+                    },
+                    {
+                        "bank_code": "RJHIMYKL",
+                        "bank_name": "Al Rajhi Banking & Investment (M) Berhad",
+                        "base_rate": 4.1,
+                        "base_financing_rate": 7,
+                        "indicative_eff_lending_rate": 5.45,
+                    },
+                    {
+                        "bank_code": "BKRMMYKL",
+                        "bank_name": "Bank Kerjasama Rakyat (M) Berhad",
+                        "base_rate": 3.85,
+                        "base_financing_rate": 6.83,
+                        "indicative_eff_lending_rate": 4.65,
+                    },
+                ],
+                "meta": {
+                    "last_updated": "2019-05-23 15:45:40",
+                    "total_result": 35,
+                    "effective_date": "2019-06-14",
+                },
+            },
+            status=200,
+        )
+        r.add(
+            responses.GET,
+            BASE_URL + "base-rate/MBBEMYKL",
+            json={
+                "data": {
+                    "bank_code": "MBBEMYKL",
+                    "bank_name": "Malayan Banking Berhad",
+                    "base_rate": 3,
+                    "base_lending_rate": 6.65,
+                    "indicative_eff_lending_rate": 4.35,
+                },
+                "meta": {
+                    "effective_date": "2019-06-14",
+                    "last_updated": "2019-05-23 15:18:34",
+                    "total_result": 1,
+                },
+            },
+            status=200,
+        )
+        r.add(
+            responses.GET,
+            BASE_URL + "base-rate/CIBBMYKL",
+            json={
+                "data": {
+                    "bank_code": "CIBBMYKL",
+                    "bank_name": "CIMB Bank Berhad",
+                    "base_rate": 4,
+                    "base_lending_rate": 6.85,
+                    "indicative_eff_lending_rate": 4.75,
+                },
+                "meta": {
+                    "effective_date": "2019-06-14",
+                    "last_updated": "2019-05-23 15:13:02",
+                    "total_result": 1,
+                },
+            },
+            status=200,
+        )
+        yield r
 
 
-def test_list_single(mock_responses):
-    b = BnmpyItem("list-single")
-    assert b.data == [{"hello": "world"}]
+class TestBnmpyItem:
+    def test_single(self, mock_basic):
+        b = BnmpyItem("single")
+        assert b.data == [{"hello": "world"}]
+
+    def test_list_single(self, mock_basic):
+        b = BnmpyItem("list-single")
+        assert b.data == [{"hello": "world"}]
+
+    def test_list_multi(self, mock_basic):
+        b = BnmpyItem("list-multi")
+        assert b.data == [{"hello": "world"}, {"foo": "bar"}]
+
+    def test_none(self, mock_basic):
+        b = BnmpyItem("none")
+        assert b.data == []
+
+    def test_list_combine(self, mock_basic):
+        b = BnmpyItem(["none", "single", "list-single", "list-multi", "fail"])
+        assert b.data == [
+            {"hello": "world"},
+            {"hello": "world"},
+            {"hello": "world"},
+            {"foo": "bar"},
+        ]
 
 
-def test_list_multi(mock_responses):
-    b = BnmpyItem("list-multi")
-    assert b.data == [{"hello": "world"}, {"foo": "bar"}]
+class TestBaseRate:
+    def test_endpoints_and_data(self, mock_base_rate):
+        br = BaseRate()
+        br_maybank = BaseRate("MBBEMYKL")
+        br_multi = BaseRate(["MBBEMYKL", "CIBBMYKL"])
 
+        assert br.endpoints == ["base-rate"]
+        assert br_maybank.endpoints == ["base-rate/MBBEMYKL"]
+        assert br_multi.endpoints == ["base-rate/MBBEMYKL", "base-rate/CIBBMYKL"]
 
-def test_none(mock_responses):
-    b = BnmpyItem("none")
-    assert b.data == []
-
-
-def test_list_combine(mock_responses):
-    b = BnmpyItem(["none", "single", "list-single", "list-multi", "fail"])
-    assert b.data == [
-        {"hello": "world"},
-        {"hello": "world"},
-        {"hello": "world"},
-        {"foo": "bar"},
-    ]
+        assert any(["MBBEMYKL" in d.values() for d in br.data])
+        assert any(["MBBEMYKL" in d.values() for d in br_maybank.data])
+        assert any(["MBBEMYKL" in d.values() for d in br_multi.data])
+        assert any(["CIBBMYKL" in d.values() for d in br.data])
+        assert any(["CIBBMYKL" in d.values() for d in br_multi.data])
+        assert any(["BKCHMYKL" in d.values() for d in br.data])
